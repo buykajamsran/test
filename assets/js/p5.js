173fef01e3c94cd60daf8df6744f6fb11239da70
buykajamsran <buyka.jamsran@gmail.com>
2022-01-10 13:45:25 -0600

    added files

---
+++ assets/js/p5.js
@@ -0,0 +1,22 @@
+let totalPts = 300;
+let steps = totalPts + 1;
+
+var canvas;
+var w = window.outerWidth;
+var h = window.outerHeight;
+function setup() {
+  canvas = createCanvas(w, h);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+  stroke(255);
+  frameRate(10);
+}
+
+function draw() {
+  background(0);
+  let rand = 0;
+  for (let i = 1; i < steps; i++) {
+    point((width / steps) * i, height / 2 + random(-rand, rand));
+    rand += random(-10, 10);
+  }
+}
